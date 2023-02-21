1da82ea366d677258d421db6c3fd0f1a2b0da2be
balandravince06 <89444982+balandravince06@users.noreply.github.com>
2023-02-21 14:25:04 +0800

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,32 @@
+<?php
+   session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+<body>
+        <center>
+        <h1>DepEd Learning Management System</h1>
+        <h4>by JCI Makati</h4>
+        <h2>Region: NCR-Manila</h2>
+        <a href="lms.html">Home</a> 
+     <a href="mission.html">Mission</a>
+     <a href="vision.html">Vision</a>
+     <a href="news.html">News</a> 
+     <a href="contactUs.html">Contact us</a>
+     <a href="grades.php">Grades</a>
+     <a href="course.php">Course</a>
+		<br>
+		<br>
+		<br>
+        </center>
+
+    
+</body>
+</html>
